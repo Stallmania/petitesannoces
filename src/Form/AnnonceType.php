1cb32980f28a85e9fa905206999991e53886d9e3
benlakhdar <benlakhdaramine@gmail.com>
2021-12-05 18:01:18 +0100

    ckeditor and elfinder

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -7,10 +7,12 @@ use App\Entity\Categories;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 
 class AnnonceType extends AbstractType
@@ -20,7 +22,7 @@ class AnnonceType extends AbstractType
         $builder
             ->add('title',TextType::class)
             //->add('slug')
-            ->add('content',TextareaType::class)
+            ->add('content', CKEditorType::class) // Ce champ sera remplacé par un éditeur WYSIWYG
             //->add('created_at')
             //->add('active')
             //->add('users')
